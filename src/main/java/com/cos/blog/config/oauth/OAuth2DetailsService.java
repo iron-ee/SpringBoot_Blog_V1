34e17b8db5add250f48a08a785b03d81154cd442
irone <doyeck@naver.com>
2021-03-16 15:09:39 +0900

    OAuth2 로그인 구현 (네이버, 카카오)

--- src/main/java/com/cos/blog/config/oauth/OAuth2DetailsService.java
+++ src/main/java/com/cos/blog/config/oauth/OAuth2DetailsService.java
@@ -1,5 +1,6 @@
 package com.cos.blog.config.oauth;
 
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.cos.blog.config.auth.PrincipalDetails;
+import com.cos.blog.domain.user.RoleType;
 import com.cos.blog.domain.user.User;
 import com.cos.blog.domain.user.UserRepository;
 
@@ -50,6 +52,10 @@ public class OAuth2DetailsService extends DefaultOAuth2UserService{
 			oAuth2UserInfo = new GoogleInfo(oAuth2User.getAttributes());
 		}else if(userRequest.getClientRegistration().getClientName().equals("Facebook")){
 			oAuth2UserInfo = new FacebookInfo(oAuth2User.getAttributes());
+		}else if(userRequest.getClientRegistration().getClientName().equals("Naver")){
+			oAuth2UserInfo = new NaverInfo((Map)(oAuth2User.getAttributes().get("response")));
+		}else if(userRequest.getClientRegistration().getClientName().equals("Kakao")){
+			oAuth2UserInfo = new KakaoInfo((Map)(oAuth2User.getAttributes()));
 		}
 		
 		// 2번 최초 : 회원가입 + 로그인,  로그인최초X : 로그인
@@ -64,6 +70,7 @@ public class OAuth2DetailsService extends DefaultOAuth2UserService{
 					.username(oAuth2UserInfo.getUsername())
 					.password(encPassword)
 					.email(oAuth2UserInfo.getEmail())
+					.role(RoleType.USER)
 					.build();
 			userEntity = userRepository.save(user);
 			System.out.println("userEntity"+userEntity);
